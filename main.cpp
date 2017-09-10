cf4181687709c96892b0a9c398d869c4cec0963a
arnav dhamija <arnav.dhamija@gmail.com>
2017-09-10 11:19:42 +0530

    More changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 #include <iostream>
+#include <stdlib.h>
 #include <math.h>
 
 #define SCREEN_WIDTH 1280
@@ -30,33 +31,106 @@ void drawLine(GLfloat start_x, GLfloat start_y, GLfloat end_x, GLfloat end_y)
     int dx = end_x - start_x;
     int dy = end_y - start_y;
 
-    if (dy > 0) {
-        int p = 2*dy - dx; //initial delta
-        int eastDelta = 2*dy;
-        int northEastDelta = 2*(dy - dx);
-        for (int x = start_x, y = start_y; x<= end_x; x++) {
-            if (p < 0) {
-                p = p + eastDelta;
-            } else {
-                p = p + northEastDelta;
-                y++;
+    if (abs(dy) > abs(dx)) {
+        if (dy > 0) {
+            int p = -2*dx + dy; //initial delta
+            int northDelta = -2*dx;
+            int northEastDelta = 2*dy - 2*dx;
+            for (int x = start_x, y = start_y; y<= end_y; y++) {
+                if (p < 0) {
+                    p = p + northDelta;
+                } else {
+                    p = p + northEastDelta;
+                    x++;
+                }
+                drawPixel(x, y);
             }
-            drawPixel(x, y);
+        } else {
+            // int p = 2*dx - dy; //initial delta
+            // int northDelta = 2*dx;
+            // int northEastDelta = 2*(dy - dx);
+            // for (int x = start_x, y = start_y; y<= end_y; y++) {
+            //     if (p < 0) {
+            //         p = p + northDelta;
+            //     } else {
+            //         p = p + northEastDelta;
+            //         x++;
+            //     }
+            //     drawPixel(x, y);
         }
     } else {
-        int p = 2*dy + dx; //initial delta
-        int eastDelta = 2*dy;
-        int southEastDelta = 2*(dy + dx);
-        for (int x = start_x, y = start_y; x<= end_x; x++) {
-            if (p > 0) {
-                p = p + eastDelta;
-            } else {
-                p = p + southEastDelta;
-                y--;
+        if (dy > 0) {
+            int p = 2*dy - dx; //initial delta
+            int eastDelta = 2*dy;
+            int northEastDelta = 2*(dy - dx);
+            for (int x = start_x, y = start_y; x<= end_x; x++) {
+                if (p < 0) {
+                    p = p + eastDelta;
+                } else {
+                    p = p + northEastDelta;
+                    y++;
+                }
+                drawPixel(x, y);
             }
-            drawPixel(x, y);
+        } else {
+            int p = 2*dy + dx; //initial delta
+            int eastDelta = 2*dy;
+            int southEastDelta = 2*(dy + dx);
+            for (int x = start_x, y = start_y; x<= end_x; x++) {
+                if (p > 0) {
+                    p = p + eastDelta;
+                } else {
+                    p = p + southEastDelta;
+                    y--;
+                }
+                drawPixel(x, y);
+            }
         }
+    }
+}
 
+void drawCirclePixel(int x, int y, int origin_x, int origin_y)
+{
+    GLfloat _x = x;
+    GLfloat _y = y;
+
+    GLfloat _ox = origin_x;
+    GLfloat _oy = origin_y;
+    // draw pixel 8 times for covering symmetry of the circle
+    drawPixel(_x+_ox, _y+_oy);
+    drawPixel(_x*-1+_ox, _y+_oy);
+    drawPixel(_x+_ox, _y*-1+_oy);
+    drawPixel(_x*-1+_ox, _y*-1+_oy);
+    drawPixel(_y+_ox, _x+_oy);
+    drawPixel(_y*-1+_ox, _x+_oy);
+    drawPixel(_y+_ox, _x*-1+_oy);
+    drawPixel(_y*-1+_ox, _x*-1+_oy);
+
+}
+
+void drawCircle(int radius, int origin_x, int origin_y)
+{
+    int x = 0;
+    int y = radius;
+
+    int d = 1 - radius;
+    int deltaE = 3;
+    int deltaSE = -2*radius + 5;
+    drawCirclePixel(x, y, origin_x, origin_y);
+
+    while(y > x) {
+        if (d < 0) { //east pixel
+            d += deltaE;
+            deltaE += 2;
+            deltaSE += 2;
+        } else { //south-east pixel
+            d += deltaSE;
+            deltaE += 2;
+            deltaSE += 4;
+            y--;
+        }
+        x++;
+        drawCirclePixel(x, y, origin_x, origin_y);
     }
 }
 
@@ -95,8 +169,18 @@ int main( void )
     {
         glClear(GL_COLOR_BUFFER_BIT);
         // bresenham(200, 100, 400, 300);
-        drawLine(400, 600, 200, 200);
-        drawLine(0, 400, 200, 200);
+        drawLine(200, 200, 200, 600);
+//        drawLine(200, 200, 200, 500);
+
+        // drawLine(200, 200, 400, 200);
+        //
+        // drawLine(200, 200, 400, 300);
+        // drawLine(200, 200, 400, 500);
+        //
+        // drawLine(0, 400, 200, 200);
+        //
+        drawPixel(300, 400);
+        drawCircle(200, 300, 400);
 
         // Swap front and back buffers
         glfwSwapBuffers( window );
