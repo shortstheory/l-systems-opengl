247ecb1c3f66daad064cd44d733b65a958727fb8
arnav dhamija <arnav.dhamija@gmail.com>
2017-09-13 23:06:22 +0530

    Updated indentation

--- lsystem_basic.cpp
+++ lsystem_basic.cpp
@@ -32,103 +32,11 @@ void drawPixel(int x, int y)
     glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-// void drawLine_error(int x0, int y0, int x1, int y1)
-// {
-//   int dx = x1 - x0;
-//   int dy = y1 - y0;
-//   int incx = 1;
-//   int incy = 1;
-//
-//   // to deal with negative slopes
-//   if (dx < 0)
-//   {
-//     dx = -dx;
-//     int temp = x0;
-//     x0 = x1;
-//     x1 = temp;
-//     temp = y0;
-//     y0 = y1;
-//     y1 = temp;
-//     // incx = -1;
-//   }
-//   if (dy < 0)
-//   {
-//     dy = -dy;
-//     // int temp = x0;
-//     // x0 = x1;
-//     // x1 = temp;
-//     // temp = y0;
-//     // y0 = y1;
-//     // y1 = temp;
-//     // incy = -1;
-//   }
-//
-//   drawPixel(x0, y0);
-//
-//   if (dx > dy) // slope -1 to 1
-//   {
-//     int d = 2 * dy - dx;
-//     int incrE = 2 * dy;
-//     int incrNE = 2 * (dy - dx);
-//
-//     int x = x0, y = y0;
-//
-//     drawPixel(x, y);
-//
-//     while (x < x1)
-//     {
-//       if (d <= 0) //East pixel chosen
-//       {
-//         d += incrE;
-//       }
-//       else // NE pixel chosen
-//       {
-//         d += incrNE;
-//         y+= incy;
-//       }
-//       x+= incx;
-//       drawPixel(x, y);
-//     }
-//   }
-//   else // exchange x and y
-//   {
-//     int d = 2 * dx - dy;
-//     int incrE = 2 * dx;
-//     int incrNE = 2 * (dx - dy);
-//
-//     int x = x0, y = y0;
-//
-//     drawPixel(x, y);
-//
-//     while (y < y1)
-//     {
-//       if (d <= 0) //East pixel chosen
-//       {
-//         d += incrE;
-//       }
-//       else // NE pixel chosen
-//       {
-//         d += incrNE;
-//         x+= incx;
-//       }
-//       y+= incy;
-//       drawPixel(x, y);
-//     }
-//   }
-// }
-
 void drawLine(int start_x, int start_y, int end_x, int end_y) // we have to take care of too many cases :P
 {
-    // printf("(%d %d) -> (%d %d)\n", start_x, start_y, end_x, end_y);
     if (start_x > end_x) {
         std::swap(start_x, end_x);
         std::swap(start_y, end_y);
-        // int temp = start_x;
-        // start_x = end_x;
-        // end_x = temp;
-        // temp = start_y;
-        // start_y = end_y;
-        // end_y = temp;
     }
     int dx = end_x - start_x;
     int dy = end_y - start_y;
@@ -197,8 +105,6 @@ pair<int, int> drawVector(int x0, int y0, int len, float angle)
 {
     int a = x0 + (int) (len * cos(angle));
     int b = y0 + (int) (len * sin(angle));
-    // printf("%d %d\n", (int) (len * cos(angle)), (int) (len * sin(angle)));
-    // printf("%d %d\n", a, b);
     drawLine(x0, y0, a, b);
     return make_pair(a, b);
 }
@@ -255,8 +161,6 @@ void drawPattern(string sentence)
             pair<int, int> temp = drawVector(x0, y0, len, rotation);
             x0 = temp.first;
             y0 = temp.second;
-            // drawLine(x0, y0, x0 + 0, y0 + len);
-            // translate(0, len);
 
             // y0 += len;
         } else if (current == '+') {
@@ -274,11 +178,7 @@ void drawPattern(string sentence)
             x0 = get<0>(temp);
             y0 = get<1>(temp);
             rotation = get<2>(temp);
-            // x0 = temp.first;
-            // y0 = temp.second;
-            // pop();
         }
-
     }
 }
 
@@ -294,15 +194,13 @@ void generateString(string sentence, int depth)
 
     len *= 0.5;
     string nextSentence;
-    // char nextSentence[1000000];
-    // strcpy(nextSentence, "");
-    // cout<<sentence.length()<<endl;
+
     for (int i = 0; i < sentence.length(); i++) {
         if (sentence[i] == 'X') {
             nextSentence += "F-[[X]+X]+F[+FX]-X";
             continue;
         }
-        if(sentence[i] == 'F') {
+        if (sentence[i] == 'F') {
             nextSentence += "FF";
             continue;
         }
@@ -315,13 +213,13 @@ void generateString(string sentence, int depth)
 
 int main()
 {
-    if(glfwInit() == false) {
+    if (glfwInit() == false) {
         fprintf(stderr, "Error initializing GLFW\n");
         return -1;
     }
 
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Example", NULL, NULL);
-    if(!window) {
+    if (!window) {
         glfwTerminate();
         fprintf(stderr, "Error while creating a window\n");
         return -1;
@@ -339,7 +237,7 @@ int main()
     generateString(sentence, 5);
 
 
-    while(!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         drawPattern(generatedString);
         glfwSwapBuffers(window);
